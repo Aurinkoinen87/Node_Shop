bce53ef9dfcd4d496384de5b5a8c54cf90c26b9b
Aurinkoinen87 <imax449@yandex.ru>
2022-03-22 05:35:02 +0300

    promise start

--- index.js
+++ index.js
@@ -1,14 +1,58 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static('public'))
-
-const port = 3000
 
 
-app.listen(port, ()=> console.log(`Server started on port ${port}`))
+
+app.use(express.static(__dirname + '/public'))
+
+app.set('view engine','pug')
+
+
+const PORT = 3000
+
+const mysql = require("mysql2");
+  
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "shop",
+  password: "root"
+});
+
+app.listen(PORT)
 
 
 app.get('/', (req, res)=> {
-    res.render('index.html')
+
+con.query(
+    'SELECT * FROM goods',
+    function(error, result){
+        if(error) throw err
+        let goods = result
+
+        res.render('main',{
+            goods
+        })
+    }
+)
 })
+
+
+app.get('/cat', (req, res)=> {
+    console.log(req.query)
+    let id = req.query.id
+
+    let category = new Promise((res, rej)=> {
+        con.query(
+            'SELECT * FROM category Where id=' + id,
+             function (error, result){
+                if(error) rej(err)
+                res(result)
+            }
+        )
+    }) 
+    })
+
+
+
